853b29e07ab50fa75d409b8ee68814de2d014d53
Rodrigo <167549133+Raynagah@users.noreply.github.com>
2024-08-13 11:51:14 -0400

    Add files via upload

---
+++ DOO_Clase 1.py
@@ -0,0 +1,31 @@
+"""
+Horarios: Martes(10:41 a 12:00) Parte Práctica | Viernes(08:31 a 09:50) Parte Teórica
+
+Asistencia Obligatoria = 70% mínimo para aprobar.
+Exámen Transversal     = 40% del ramo con 60% de exigencia.
+
+Parcial 1 = 10 DE SEPTIEMBRE | 30% del ramo (de ese 30% el 80% será la prueba y el 20% será por realización de tareas).
+Parcial 2 = 22 DE OCTUBRE    | 40% del ramo (de ese 40% el 80% será la prueba y el 20% será por realización de tareas).
+Parcial 3 = 26 DE NOVIEMBRE  | 30% del ramo.(100% prueba).
+
+Parcial 1 Temario:
+    Clases: Generalización de un objeto.
+    Atributos: Característica o dato.
+    Objetos: Especificación o cosa.
+Parcial 2 Temario:
+    Herencia: Elementos o características que se comparten entre clases superiores a inferiores.
+Parcial 3 Temario:
+    Presentación de índole semiformal en grupos de 3 integrantes. Dentro de la misma se requiere mostrar los siguientes items:
+        - Interfaz | Modelo de Base de Datos (insertar, eliminar, buscar y actualizar datos)| Controlador
+    Modelo Vista Controlador luego Modelo Plantilla Controlador se le llama a estos proyectos.
+SQL será el lenguaje a utilizar en base de datos.
+MySQL.
+SQLServer.
+
+Instalarse Netbeans IDE 18 (evitar la 19). Adicionalmente, MySQL Workbench 8.0 CE.
+
+
+
+
+
+"""
